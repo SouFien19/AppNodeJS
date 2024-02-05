ba29b4dc9a14fa328f5324039e2d698f4ea188ae
SouFien19 <soufienlabiadh19@gmail.com>
2024-02-05 16:01:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample data for /post/all
+const posts = [
+  { id: 1, name: 'soyfien', lastname: 'labiadh' },
+  { id: 2, name: 'ghaith', lastname: 'labiadh' },
+];
+
+// Route for /auth/login
+app.get('/auth/login', (req, res) => {
+
+  res.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.get('/auth/register', (req, res) => {
+
+  res.sendFile(path.join(__dirname + '/register.html'));
+});
+
+// Route for /post/all
+app.get('/post/all', (req, res) => {
+  // Return the array of posts as JSON
+  res.json(posts);
+});
+
+// Route for /post/:id
+app.get('/post/:id', (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find(post => post.id === postId);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send('Post not found');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
